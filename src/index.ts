8182efaaa1330e8c3cc2135339e7cb6e17a1c9fc
snqre <marco.bizxaro@gmail.com>
2026-07-06 21:51:35 +0200

    update tab icon

--- src/index.ts
+++ src/index.ts
@@ -1,20 +1,28 @@
+import * as Path from "path";
+import * as Process from "process";
 import html from "./index.html";
 
 Bun.serve({
 	port: 8080,
 	fetch: request => {
 		let url: URL = new URL(request.url);
-		
+
 		switch (url.pathname) {
 			case "/": return new Response(html as unknown as Bun.BodyInit, {
 				headers: {
 					"Content-Type": "text/html"
 				}
 			});
-			
-			case "/another-path": break
 		}
-		
+
+		if (url.pathname.startsWith("/asset/")) {
+			let cwd: string = Process.cwd();
+			let path: string = Path.join(cwd, "src", url.pathname);
+			let file: Bun.BunFile = Bun.file(path);
+
+			return new Response(file);
+		}
+
 		return new Response("not found", {
 			status: 404
 		});
